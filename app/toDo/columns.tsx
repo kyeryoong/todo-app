55a330e0815d3d593a5738dfd97e1343b2508934
kyeryoong <kyeryoong@gmail.com>
2025-02-06 20:49:27 +0900

    feat: DataTable 기본 구현

---
+++ app/toDo/columns.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { ToDoType } from "@/type/todo";
+import { ColumnDef } from "@tanstack/react-table";
+
+export const columns: ColumnDef<ToDoType>[] = [
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "priority",
+    header: "Priority",
+  },
+  {
+    accessorKey: "completed",
+    header: "Completed",
+  },
+];
